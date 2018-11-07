1d2122e8fcffed1621459a7eeac0b33d3679010d
Liu Qun <qunliu@zyhx-group.com>
2018-11-07 20:16:50 +0800

    更新spark内存配置及IP地址指定

--- spark-conf-files/spark-env.sh
+++ spark-conf-files/spark-env.sh
@@ -27,8 +27,8 @@
 # - SPARK_PUBLIC_DNS, to set the public dns name of the driver program
 
 # Options read by executors and drivers running inside the cluster
-# - SPARK_LOCAL_IP, to set the IP address Spark binds to on this node
-# - SPARK_PUBLIC_DNS, to set the public DNS name of the driver program
+export SPARK_LOCAL_IP="192.168.1.200"
+export SPARK_PUBLIC_DNS="192.168.1.200"
 # - SPARK_LOCAL_DIRS, storage directories to use on this node for shuffle and RDD data
 # - MESOS_NATIVE_JAVA_LIBRARY, to point to your libmesos.so if you use Mesos
 
@@ -38,10 +38,11 @@
 # - YARN_CONF_DIR, to point Spark towards YARN configuration files when you use YARN
 # - SPARK_EXECUTOR_CORES, Number of cores for the executors (Default: 1).
 # - SPARK_EXECUTOR_MEMORY, Memory per Executor (e.g. 1000M, 2G) (Default: 1G)
+export SPARK_EXECUTOR_MEMORY="890m"
 # - SPARK_DRIVER_MEMORY, Memory for Driver (e.g. 1000M, 2G) (Default: 1G)
 
 # Options for the daemons used in the standalone deploy mode
-# - SPARK_MASTER_HOST, to bind the master to a different IP address or hostname
+export SPARK_MASTER_HOST="192.168.1.200"
 # - SPARK_MASTER_PORT / SPARK_MASTER_WEBUI_PORT, to use non-default ports for the master
 # - SPARK_MASTER_OPTS, to set config properties only for the master (e.g. "-Dx=y")
 # - SPARK_WORKER_CORES, to set the number of cores to use on this machine
@@ -55,7 +56,7 @@
 # - SPARK_DAEMON_JAVA_OPTS, to set config properties for all daemons (e.g. "-Dx=y")
 # - SPARK_DAEMON_CLASSPATH, to set the classpath for all daemons
 # - SPARK_PUBLIC_DNS, to set the public dns name of the master or workers
-export SPARK_MASTER_HOST="master"
+export SPARK_MASTER_HOST="192.168.1.200"
 export SPARK_WORKER_CORES=1
 export SPARK_WORKER_MEMORY="900m"
 export SPARK_DAEMON_MEMORY="900m"
