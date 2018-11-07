4149a769be97663c5f3237ccdc43840eca276e56
刘群 <qunliu@zyhx-group.com>
2018-11-07 18:57:48 +0800

    整理代码调整对齐

--- my_pyspark_script.py
+++ my_pyspark_script.py
@@ -69,25 +69,6 @@ def init_tf_session():
     return session2, local_vars
 
 
-def do_tf_warming_up():
-    test_str = """6.540000000000000036e+00 3.271000000000000085e+01 6.540999999999999659e+01 9.893000000000000682e+01 1.275499999999999972e+02 1.618899999999999864e+02 1.815099999999999909e+02 2.003199999999999932e+02 2.207599999999999909e+02 2.379300000000000068e+02 2.681800000000000068e+02 2.820799999999999841e+02 3.008899999999999864e+02 3.139700000000000273e+02 3.049700000000000273e+02 3.033399999999999750e+02 3.025199999999999818e+02 3.041600000000000250e+02 3.025199999999999818e+02 3.016999999999999886e+02 3.041600000000000250e+02 3.016999999999999886e+02 3.008899999999999864e+02 3.000699999999999932e+02 3.000699999999999932e+02 2.984300000000000068e+02 3.008899999999999864e+02 2.910699999999999932e+02 2.714499999999999886e+02 2.485600000000000023e+02 2.158499999999999943e+02 1.888700000000000045e+02 1.610699999999999932e+02 1.250999999999999943e+02 8.993999999999999773e+01 5.560000000000000142e+01 2.698000000000000043e+01 0.000000000000000000e+00 -3.025000000000000000e+01 -5.886999999999999744e+01 -8.829999999999999716e+01 -1.250999999999999943e+02 -1.504399999999999977e+02 -1.766100000000000136e+02 -2.019499999999999886e+02 -2.142199999999999989e+02 -2.330200000000000102e+02 -2.526500000000000057e+02 -2.747200000000000273e+02 -2.918899999999999864e+02 -3.074300000000000068e+02 -3.098799999999999955e+02 -3.098799999999999955e+02 -3.082500000000000000e+02 -3.074300000000000068e+02 -3.090600000000000023e+02 -3.074300000000000068e+02 -3.098799999999999955e+02 -3.049700000000000273e+02 -3.057900000000000205e+02 -3.049700000000000273e+02 -2.886200000000000045e+02 -3.041600000000000250e+02 -3.066100000000000136e+02 -3.008899999999999864e+02 -2.853500000000000227e+02 -2.616399999999999864e+02 -2.420200000000000102e+02 -2.101299999999999955e+02 -1.790600000000000023e+02 -1.479900000000000091e+02 -1.079300000000000068e+02 -7.521999999999999886e+01 -4.906000000000000227e+01 -8.990000000000000213e+00 8.179999999999999716e+00 3.760999999999999943e+01 7.195000000000000284e+01 1.054699999999999989e+02 1.357299999999999898e+02
-"""
-    raw_data = np.fromstring(test_str, dtype=np.float64, sep=" ")
-    print(raw_data)
-    my_reshaped_data = np.reshape(raw_data, (1, 1, 80))
-    print(my_reshaped_data)
-    sess, my_vars = init_tf_session()
-    with sess:
-        results = sess.run(my_vars.y, feed_dict={my_vars.x: my_reshaped_data})
-    print(results)
-
-
-#def ch_split(line):
-#    return line.split(" ")
-
-# .map(lambda lines: lines.split("\n"))
-
-
 def reshape_ndarray(a):
     print('== Debug: timestamp =', datetime.now())
 
@@ -133,17 +114,6 @@ def do_result_counting(uncounted_result_list):
     return cnt
 
 
-#def output_text(rdd):
-#    v = rdd.collect()
-#    if len(v) < 1:
-#        print('Debug: No data inputed now, timestamp =', datetime.now())
-#        return
-#    print(v)
-#    a = v[0]
-#    total_cnt = sum(a)
-#    print('Debug: Computed records:', total_cnt)
-#    print('Debug: Completed at', datetime.now())
-
 global APPLIANCE_NAME_TABLE_GBK
 APPLIANCE_NAME_TABLE_GBK = [utf8.decode('utf-8').encode('gbk') for utf8 in APPLIANCE_NAME_TABLE]
 
@@ -161,8 +131,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: network_wordcount.py <hostname> <port>", file=sys.stderr)
         exit(-1)
-    print('skip warming-ups...')
-    #do_tf_warming_up()
     sc = SparkContext(appName="TestModelClassify")
     BATCH_DURATION_SECONDS = 10
     ssc = StreamingContext(sc, BATCH_DURATION_SECONDS)
@@ -185,11 +153,11 @@ if __name__ == "__main__":
     def my_print(time, rdd):
         def send_msg(iter):
             udpout = socket.socket(AF_INET, SOCK_DGRAM)
-	    udpout.sendto(b''.join(('=== ', time.strftime('%Y-%m-%d %H:%M:%S'), ' ===\n\r')), udpremotehostport)
+            udpout.sendto(b''.join(('=== ', time.strftime('%Y-%m-%d %H:%M:%S'), ' ===\n\r')), udpremotehostport)
             for sorted_table in iter:
                 gbkmsg = b''.join((gbk_msg_from_table(sorted_table), '\n\r'))
                 udpout.sendto(gbkmsg, udpremotehostport)
-	    udpout.sendto(b'\n\r', udpremotehostport)
+            udpout.sendto(b'\n\r', udpremotehostport)
             udpout.close()
         if rdd.count() > 0:
             rdd.foreachPartition(send_msg)
