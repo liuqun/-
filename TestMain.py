a1d9465c6d2d6bf55cabcf111b293e687ba4b55f
刘群 <qunliu@zyhx-group.com>
2018-11-07 17:54:57 +0800

    改进模型加载的代码实现方式
    代码重构

--- TestMain.py
+++ TestMain.py
@@ -3,6 +3,7 @@
 import tensorflow as tf
 import numpy as np
 import os
+import contextlib
 
 class MyException(Exception):
     pass
@@ -37,6 +38,60 @@ def model_predict(chk_path, data):
         return sess.run(y, feed_dict)
 
 
+class MyClassifierModel:
+    sess = None
+    x = None
+    y = None
+
+    def __init__(self, sess, x, y):
+        self.x = x
+        self.y = y
+        self.sess = sess
+
+    def predict(self, data):
+        """使用当前会话执行一次预测
+
+        :param data: 待预测数据, 矩阵形状必须是 n*1*80
+        :type data: numpy.ndarray
+        :return: 预测的负荷索引
+        :rtype : numpy.ndarray
+        """
+        return self.sess.run(self.y, feed_dict={self.x: data})
+
+
+class ModelFileLoader:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    @contextlib.contextmanager
+    def open(ckpt_dir):
+        if not os.path.isdir(ckpt_dir):
+            raise ModelLoadingError('Invalid path "{}"'.format(ckpt_dir))
+        latest_checkpoint_str = tf.train.latest_checkpoint(ckpt_dir)
+        if not latest_checkpoint_str:
+            raise ModelLoadingError('Checkpoint file not found in dir "{}"'.format(ckpt_dir))
+        meta_graph_filename = latest_checkpoint_str + '.meta'
+        graph = tf.Graph()
+        try:
+            with graph.as_default():
+                saver = tf.train.import_meta_graph(meta_graph_filename)
+        except IOError:
+            raise ModelLoadingError('Meta graph file "{}" is missing'.format(meta_graph_filename))
+        x = graph.get_tensor_by_name("x:0")
+        logits = graph.get_tensor_by_name("logits_eval:0")
+        y = tf.argmax(logits, 1)
+        sess = tf.Session(graph=graph)
+        saver.restore(sess, latest_checkpoint_str)
+        pre_trained_model = MyClassifierModel(sess, x, y)
+        yield pre_trained_model
+        sess.close()
+
+
+class ModelLoadingError(Exception):
+    pass
+
+
 def print_output_index(output_index):
     if not output_index.any():
         print('预测失败')
@@ -68,9 +123,10 @@ if __name__ == '__main__':
 
     # 模型预测
     try:
-        output_index = model_predict(path_for_check, rsp_test_data)
-        print_output_index(output_index)  # 结果输出
-    except MyException as e:
+        with ModelFileLoader.open(path_for_check) as model:
+            output_index = model.predict(rsp_test_data)
+        print_output_index(output_index)
+    except ModelLoadingError as e:
         print('Debug: Failed:', e)
         exit(127)
 
