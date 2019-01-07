d01cbdeb4559aa2f35556c2e5d126a0a3d2ec601
Liu Qun <qunliu@zyhx-group.com>
2019-01-07 12:57:27 +0800

    更新pyspark脚本命令行执行说明

--- my_pyspark_script.py
+++ my_pyspark_script.py
@@ -1,31 +1,10 @@
 #!/usr/bin/env python2
 # -*- endcoding: utf-8 -*-
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 r"""
- Counts words in UTF8 encoded, '\n' delimited text received from the network every 20 seconds.
- Usage: network_wordcount.py <hostname> <port>
-   <hostname> and <port> describe the TCP server that Spark Streaming would connect to receive data.
-
  To run this on your local machine, you need to first run a Netcat server
     `$ nc -lk 9999`
  and then run the example
-    `$ bin/spark-submit examples/src/main/python/streaming/network_wordcount.py localhost 9999`
+    `$ bin/spark-submit path/to/my_pyspark_script.py localhost 9999`
 """
 from __future__ import print_function
 from __future__ import division
@@ -162,7 +141,7 @@ def gbk_msg_from_table(table):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: network_wordcount.py <hostname> <port>", file=sys.stderr)
+        print("Usage: {script} <hostname> <port>".format(script=sys.argv[0]), file=sys.stderr)
         exit(-1)
     sc = SparkContext(appName="TestModelClassify")
     BATCH_DURATION_SECONDS = 10
